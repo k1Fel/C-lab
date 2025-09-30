75844b224159be8e59ef480828bbc1d1ec1e7ce1
k1Fel <rsdarx@gmail.com>
2025-09-30 21:30:54 +0300

    Create TASK 1

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+namespace Task1
+{
+    public class Program
+    {
+
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+                return "Двері відкриваються!";
+            else
+                return "Двері зачинені...";
+        }
+
+        public static void Main()
+        {
+            Console.Write("Введіть число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            string message = GetMessage(number);
+            Console.WriteLine(message);
+        }
+
+    }
+}
