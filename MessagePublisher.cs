4d7b9d6a309568b20a27eb820b6fc0c5a9672924
k1Fel <rsdarx@gmail.com>
2026-03-25 12:15:36 +0200

    Done

---
+++ MessagePublisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starodubov_PD21
+{
+    public class MessagePublisher
+    {
+        public event Action<string> MessagePublished;
+
+        public void PublishMessage(string message)
+        {
+            MessagePublished?.Invoke(message);
+        }
+    }
+}
